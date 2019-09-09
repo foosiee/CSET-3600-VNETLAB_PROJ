1f16798abf7672b6a62d6a5592a13661f97a2be5
foosiee <colefoos27@hotmail.com>
2019-09-09 14:30:47 -0400

    Created Example GUI

---
+++ src/classes/Driver.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Driver
+{
+    public static void main(String[] args) 
+    {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Window.createAndShow();
+            }
+        });
+    }
+}
---
+++ src/classes/Window.java
@@ -0,0 +1,120 @@
+package classes;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.util.*;
+import java.net.URI;
+import java.net.URL;
+
+public class Window extends JPanel implements ListSelectionListener 
+{
+    private JLabel device;
+    private JList list;
+    private JSplitPane pane;
+    private String[] deviceNames = {"Router", "Virtual Machine"};
+    private Map<String, String> fileNames = new HashMap<>()
+    {
+        {
+            put("Virtual Machine", "vm");
+            put("Router", "router");
+        }
+    };
+
+    public Window()
+    {
+        list = new JList(deviceNames);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setSelectedIndex(0);
+        list.addListSelectionListener(this);
+
+        JScrollPane listScrollPane = new JScrollPane(list);
+        device = new JLabel();
+        device.setFont(device.getFont().deriveFont(Font.ITALIC));
+        device.setHorizontalAlignment(JLabel.CENTER);
+
+        JScrollPane deviceScrollPane = new JScrollPane(device);
+
+        pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                                  listScrollPane, deviceScrollPane);
+        pane.setOneTouchExpandable(true);
+        pane.setDividerLocation(150);
+
+        Dimension minimumSize = new Dimension(100, 50);
+        listScrollPane.setMinimumSize(minimumSize);
+        deviceScrollPane.setMinimumSize(minimumSize);
+
+        pane.setPreferredSize(new Dimension(800, 400));
+        updateLabel(deviceNames[list.getSelectedIndex()]);
+    }
+
+    public void valueChanged(ListSelectionEvent e) 
+    {
+        JList list = (JList)e.getSource();
+        updateLabel(deviceNames[list.getSelectedIndex()]);
+    }
+
+    protected String getFileName(String objectName)
+    {
+        return fileNames.get(objectName);
+    }
+
+    protected void updateLabel (String name)
+     {
+        name = getFileName(name);
+        ImageIcon icon = createImageIcon("/src/assets/" + name + ".png");
+        device.setIcon(icon);
+        if (icon != null) {
+            device.setText(null);
+        } else {
+            device.setText("Image not found");
+        }
+    }
+
+    protected static URL getFileUrl(String path)
+    {
+        try{
+            String dirName = System.getProperty("user.dir");
+            path = dirName + path;
+            System.out.println((path));
+            File f = new File(path);
+            return f.toURI().toURL();
+        }
+        catch (Exception MalformedURLException){
+            return null;
+        }
+    }
+
+   
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected static ImageIcon createImageIcon(String path)
+    {
+        URL imgURL = getFileUrl(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    public static void createAndShow()
+    {
+        JFrame frame = new JFrame("VNETLAB");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Window window = new Window();
+        frame.getContentPane().add(window.getSplitPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public JSplitPane getSplitPane() {
+        return pane;
+    }
+
+}
